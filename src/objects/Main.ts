6e82acf58566aeefa1b9d527cc6cecc0fb6fc0b3
Lucas M. Rech <lrech@imply.com>
2020-05-21 12:24:11 -0300

    Initial commit.

--- src/objects/Main.ts
+++ src/objects/Main.ts
@@ -2,6 +2,8 @@ import {CacheStore} from '../objects/CacheStore';
 
 export class Main {
 
+  private loadingTimeout;
+
   onError(exception) {
     this.getCache().addObject('loading', false);
     console.log(exception);
@@ -9,6 +11,16 @@ export class Main {
 
   setLoading(bol) {
     this.getCache().addObject('loading', bol);
+    if (bol) {
+      this.loadingTimeout = setTimeout(function() {
+        this.setLoading(false);
+        console.log('Loading timed out.');
+      }.bind(this), 5000);
+    } else {
+      if (this.loadingTimeout !== null) {
+        clearTimeout(this.loadingTimeout);
+      }
+    }
   }
 
   getCache() {
